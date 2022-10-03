adb9cba830568dc73cf1ad27da54a7b250f2162c
yang1666 <yangyimingorelena@gmail.com>
2022-10-03 15:53:06 -0400

    implement signin api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,10 +8,10 @@ import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import App from './App';
+import App from './App.js';
 
 import reportWebVitals from './reportWebVitals';
-import { StoreProvider } from './Store';
+import { StoreProvider } from './Store.js';
 
 // const root = //ReactDOM.createRoot(document.getElementById('root'));
 
